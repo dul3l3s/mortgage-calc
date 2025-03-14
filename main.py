a53b6360a5e89e797b2ab647beed19e49abec94f
dul3l3s <wightnick@hotmail.com>
2025-03-13 21:14:06 -0500

    updated defaults

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ st.title("Mortgage Repayments Calculator")
 
 st.write("### Input Data")
 col1, col2 = st.columns(2)
-home_value = col1.number_input("Home Value", min_value=0, value=500000)
-deposit = col1.number_input("Deposit", min_value=0, value=100000)
-interest_rate = col2.number_input("Interest Rate (in %)", min_value=0.0, value=5.5)
+home_value = col1.number_input("Home Value", min_value=0, value=180000)
+deposit = col1.number_input("Deposit", min_value=0, value=5000)
+interest_rate = col2.number_input("Interest Rate (in %)", min_value=0.0, value=6.75)
 loan_term = col2.number_input("Loan Term (in years)", min_value=1, value=30)
 
 # Calculate the repayments.
